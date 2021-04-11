cca633f31a86f6d727bbfd7a54a2b23c17195d0b
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-04-11 11:02:24 +0530

    Update #4 enum in solidity.sol

--- #4 enum in solidity.sol
+++ #4 enum in solidity.sol
@@ -22,3 +22,15 @@ contract MyContract{
     }
 
 }
+
+
+/*
+   enum has the 3 values Waiting,Ready,Active.  [0,1,2]
+   A constructor is used to defaultly set the value as Waiting.
+   
+   activate() allows the state to become Active[2]
+   
+   isActive() checks if the state is active or not
+   
+   ready() allows the state to become Ready[1]
+*/

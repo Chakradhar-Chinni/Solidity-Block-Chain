7891cf6abd0ab793a29172a9e0ed5c4bd7bec371
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-04-11 10:11:18 +0530

    Create constant in solidity.sol

---
+++ constant in solidity.sol
@@ -0,0 +1,11 @@
+To set a constant value to a variable
+
+pragma solidity 0.5.1;
+
+contract MyContract{
+    string public constant value= "123Value";
+
+}
+
+
+The above value can be changed from the IDE and deployed again to get the updated value
